73dc717a96ac8dd70311424678801aca1b62f0fd
fh222dt <fh222dt@student.lnu.se>
2016-06-08 11:57:12 +0200

    tag directive

--- components/map/mapController.js
+++ components/map/mapController.js
@@ -1,10 +1,34 @@
-app.controller('MapController', function($scope, NgMap) {
+app.controller('MapController', function(NgMap) {
+    var vm = this;
 
     NgMap.getMap().then(function(map) {
-        $scope.map = map;
-  });
+        vm.map = map;
+    });
 
-  $scope.infoWindow = function(event, place) {
+    vm.showDetail = function (e, place) {
+          vm.place = place;
+          //vm.map.showInfoWindow('foo-iw', p.locationId);
+          vm.map.showInfoWindow('foo-iw', this);
+        };
 
-  };
+        vm.hideDetail = function () {
+           vm.map.hideInfoWindow('foo-iw');
+        };
+
+
+
+  // vm.showDetail = function(event, place) {
+  //     var infoWindow = new google.maps.infoWindow();
+  //     var center = new google.maps.LatLng(place.latitude, place.longitude);
+  //
+  //     infoWindow.setContent(
+  //         '<h3>Hej</h3>');
+  //
+  //   infoWindow.setPosition(center);
+  //   infoWindow.open(vm.objMapa);
+  //
+  //
+  //   //   vm.place = place;
+  //   //   vm.map.showInfoWindow('foo-iw', this);
+  // };
 });
--- components/places/controllers/allPlacesController.js
+++ components/places/controllers/allPlacesController.js
@@ -1,7 +1,15 @@
-app.controller('AllPlacesController', function($scope, $rootScope, PlacesService) {
+app.controller('AllPlacesController', function($scope, $rootScope, PlacesService, TagsService) {
     PlacesService.getAllPlaces()
         .success(function(data) {
         $scope.allPlaces = data.places;
+        })
+        .error(function(data) {
+            console.log(data);
+        });
+
+    TagsService.getAllTags()
+        .success(function(data) {
+        $scope.allTags = data.tags;
         })
         .error(function(data) {
             console.log(data);
