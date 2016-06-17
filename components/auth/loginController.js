1946ce95afc794987b00df1f00f0c6b89e72eccd
fh222dt <fh222dt@student.lnu.se>
2016-06-17 12:00:56 +0200

    fixing token

--- components/auth/loginController.js
+++ components/auth/loginController.js
@@ -9,14 +9,13 @@ app.controller('LoginController', function(AuthService, $location, Flash, $rootS
         var promise = AuthService.login(vm.email, vm.password);
         //on success we logg in and store the token we get back
         promise.then(function(data, status, headers, config) {
-
-            $rootScope.token = data.jwt;
+            $rootScope.token = data.data.jwt;
             $rootScope.isLoggedIn = true;
             $location.path('/');
         });
 
         promise.catch(function(data, status, headers, config) {
-            Flash.create('warning', data.error);
+            Flash.create('warning', 'Det blev ett fel när du loggade in: ' +data.error);
 
             $rootScope.token = 'Det blev ett fel när du loggade in: ' +data.error;
             $rootScope.isLoggedIn = false;
